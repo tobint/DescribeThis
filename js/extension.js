fdff0d50af022eb1d14a31dfdf24ef188a4b6f58
tobint <tobin.titus@gmail.com>
2017-07-26 16:18:35 -0700

    Renaming options.js to distinguise between type js files. Isolating extension into it's own file.

---
+++ js/extension.js
@@ -0,0 +1,74 @@
+function Extension() {
+    var _Storage = null;
+    var _Options = null;
+    var _Cognitive = null;
+    var _Setup = false;
+
+    function _getOptionsCallback(settings) {
+        if (settings.visionApiKey == null) {
+            abandon("set options first");
+        } else {
+            _Options = settings;
+            _Cognitive = CognitiveServices(_Options);
+            _Setup = true;
+        }
+    }
+
+    function abandon(message) {
+        throw message; //TODO: Change to something more meaningful
+    };
+
+    function processImage(sourceImageUrl) {
+        var returnObj = {
+            "caption": "",
+            "confidence": 0,
+            "people": 0,
+            "emotions": []
+        };
+
+        var analyze = _Cognitive.VisionAnalyze(sourceImageUrl);
+
+        returnObj.caption = analyze.description.captions[0].text;
+        returnObj.confidence = (parseInt(analyze.description.captions[0].confidence * 1000) / 10);
+
+        if (analyze.hasPerson) {
+
+            // If it's a person, get their emotion
+            var people = _Cognitive.EmotionRecognize(sourceImageUrl);
+            var peopleLength = people.length;
+
+            returnObj.people = peopleLength;
+
+            if (peopleLength > 0) {
+                for (var p = 0; p < peopleLength; p++) {
+                    var scores = people[p].scores;
+                    var items = Object.keys(scores).map(
+                        function (key) { return [key, scores[key]]; });
+                    items.sort(
+                        function (first, second) { return second[1] - first[1]; });
+                    var emotions = (items.slice(0, 1));
+                    returnObj.emotions.push(emotions[0][0]);
+                }
+            }
+        }
+        return returnObj;
+    };
+
+
+    function setupExtension() {
+        if (_Setup) return;
+
+        _Storage = ExtensionStorage(this);
+        // Load options
+        if (_Options == null) {
+            _Storage.getAllOptionSettings(_getOptionsCallback);
+        }
+
+    }
+
+    return {
+        "abandon": abandon,
+        "processImage": processImage,
+        "setup": setupExtension
+    };
+}
