e10837983c93027abf69e8eca367be522518e0b8
Tobin Titus <tobin.titus@gmail.com>
2017-07-25 18:50:57 -0700

    Closes #1 - Add options page for entering subscription keys

---
+++ js/options.js
@@ -0,0 +1,94 @@
+var displayType = {
+    error: 1,
+    information: 2,
+    success: 3
+};
+
+function load(event) {
+    document.getElementById("saveOptions").addEventListener("click", saveOptions, false);
+    getOptions();
+}
+
+function getOptions() {
+    let settings = browser.storage.sync.get(
+        [   "visionApiRegion",
+            "visionApiKey",
+            "emotionApiRegion",
+            "emotionApiKey"
+        ], storageGetComplete);
+}
+
+function storageGetComplete(settings) {
+    var error = browser.runtime.lastError;
+
+    if (error) {
+        displayGetError();
+    } else {
+
+        // Get Congitive Services Settings
+        document.getElementById("visionApiRegion").value = settings.visionApiRegion;
+        document.getElementById("visionApiKey").value = settings.visionApiKey;
+        document.getElementById("emotionApiRegion").value = settings.emotionApiRegion;
+        document.getElementById("emotionApiKey").value = settings.emotionApiKey;
+    }
+}
+
+function displayGetError(settings) {
+    console.log("Error getting settings.");
+}
+
+function saveOptions(event) {
+    let setting = browser.storage.sync.set(
+        {
+            /* Cognitive Services Settings */
+            "visionApiRegion": document.getElementById("visionApiRegion").value,
+            "visionApiKey": document.getElementById("visionApiKey").value,
+            "emotionApiRegion": document.getElementById("emotionApiRegion").value,
+            "emotionApiKey": document.getElementById("emotionApiKey").value,
+
+        }, storageSetComplete);
+}
+
+function storageSetComplete() {
+    var error = browser.runtime.lastError;
+
+    if (error) {
+        displaySaveError(error);
+    } else {
+        displaySaveSuccess();
+    }
+
+}
+
+function displaySaveError(error) {
+    setDisplay(displayType.error, "Unable to save settings: " + error);
+}
+
+function displaySaveSuccess() {
+    setDisplay(displayType.success, "Settings saved");
+
+}
+
+function setDisplay(dispType, text) {
+    var displayBox = document.getElementById("displayBox");
+    var infoBox = document.getElementById("userInformation");
+    infoBox.innerHTML = text;
+
+    switch (dispType) {
+        case displayType.error:
+            infoBox.className = "infoBoxError";
+            break; 
+        case displayType.information:
+            infoBox.className = "infoBoxInformation";
+            break;
+        case displayType.success:
+            infoBox.className = "infoBoxSuccess";
+            break;
+        default:
+            break;
+    } 
+    displayBox.classList.remove("hidden");
+    setTimeout(function () { displayBox.classList.add("hidden"); }, 2000);
+}
+
+window.addEventListener("load", load, false);
