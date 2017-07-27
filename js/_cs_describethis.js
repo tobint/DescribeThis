76295241f512da3c60e1b48b91a37a6d94e5fc0e
tobint <tobin.titus@gmail.com>
2017-07-26 18:42:08 -0700

    Start work on recognizing background Images

--- js/_cs_describethis.js
+++ js/_cs_describethis.js
@@ -1,9 +1,20 @@
 var images = document.images;
 
 for (var image in images) {
-    images[image].addEventListener("mouseover", function (e) {
-        // alert(e.currentTarget.src);
-        
+    images[image].addEventListener("mouseover", function (e) {  
         browser.runtime.sendMessage(e.currentTarget.src);
     }, false);
 }
+
+/*
+var anchors = document.getElementsByTagName("a");
+for (var anchor in anchors) {
+    var backgroundImage = anchors[anchor].style.backgroundImage;
+
+    if (backgroundImage != "") {
+        anchors[anchor].addEventListener("mouseover", function (e) {
+            browser.runtime.sendMessage(backgroundImage);
+        }, false);
+    }
+}
+*/
